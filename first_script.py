454ba322afc60e110df2d1b918d364db0bd6ae3d
Karena <khuang@mills.edu>
2019-02-06 16:07:39 -0800

    second update?

---
+++ first_script.py
@@ -0,0 +1,14 @@
+"""
+Karena Huang
+CS80K Python
+2/7/19
+first_script.py
+"""
+print("Hello! What is your name?")
+name = input()
+name_length = len(name)
+print("Could you also tell me your age?")
+age = input()
+
+print('Oh! So your name is ' + name + ' and your age is ' + str(age) + '.')
+print('Your name has ' + str(name_length) + ' characters.')
---
+++ nested_if.py
@@ -0,0 +1,10 @@
+num = 10
+mynum = 5
+
+if num == mynum:
+    print('You are lucky!!')
+else:
+    if num < mynum:
+        print('Too low')
+    else:
+        print('Too high')
