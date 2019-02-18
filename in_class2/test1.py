8c2f97c26070c9293a0b421b2d075948c02a1b54
Karena <khuang@mills.edu>
2019-02-17 16:48:22 -0800

    python class exam1 test case

--- in_class2/test1.py
+++ in_class2/test1.py
@@ -13,28 +13,28 @@ age = int(age)
 
 try:
 #if user inputs with the bday variable any of these strings, do this
-if (bday == "N" or bday == "n" or bday == "no" or bday == "No"):
-    #create new variable yearBorn calculate year born without factoring in
-    #this year, so add 1
-    yearBorn = year - (age + 1)
-    #cast yearBorn into a string so it's possible to include in print statement
-    #later
-    yearBorn = str(yearBorn)
-    #print out what we need
-    print("Hello " + name + ", you were born in the year " + yearBorn + "\n" +
-          "Have a great birthday this year!")
+    if (bday == "N" or bday == "n" or bday == "no" or bday == "No"):
+        #create new variable yearBorn calculate year born without factoring in
+        #this year, so add 1
+        yearBorn = year - (age + 1)
+        #cast yearBorn into a string so it's possible to include in print statement
+        #later
+        yearBorn = str(yearBorn)
+        #print out what we need
+        print("Hello " + name + ", you were born in the year " + yearBorn + "\n" +
+              "Have a great birthday this year!")
 
-#if user inputs with the bday variable any of these strings, do this
-elif (bday == "Y" or bday == "y" or bday == "yes" or bday == "Yes"):
-    #declare new variable yearBorn calculate yearborn factoring in
-    #current year
-    yearBorn = year - age
-    #cast yearBorn as string to include in print statement later
-    yearBorn = str(yearBorn)
-    #print what we need
-    print("Hello " + name + ", you were born in the year " + yearBorn + "\n" +
-          "Happy delayed birthday.")
+    #if user inputs with the bday variable any of these strings, do this
+    elif (bday == "Y" or bday == "y" or bday == "yes" or bday == "Yes"):
+        #declare new variable yearBorn calculate yearborn factoring in
+        #current year
+        yearBorn = year - age
+        #cast yearBorn as string to include in print statement later
+        yearBorn = str(yearBorn)
+        #print what we need
+        print("Hello " + name + ", you were born in the year " + yearBorn + "\n" +
+              "Happy delayed birthday.")
     
 #if user inputs anything else into the bday variable, print out wrong answer
-else:
+except:
     print("Wrong answer")
